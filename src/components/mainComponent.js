064e330a4ebbee49073f612fb7ff9d00a76aa5e9
luis Ramirez <l.r.32@hotmail.com>
2020-02-12 11:27:34 -0800

    website before router with dishes as homepage footer and header

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Home from './homeComponent';
 import Menu from './menuComponent';
 import DishDetail from './dishDetailComponent';
 import Header from './headerComponent';
 import Footer from './footerComponent';
 import { DISHES } from '../shared/dishes';
-import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
@@ -13,28 +11,23 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      selectedDish: null
     };
   }
 
+    onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId});
+  }
 
 
   render() {
-    
-       const HomePage = () => {
-      return(
-          <Home 
-          />
-      );
-    }
-    
      return (
     <div>
         <Header />
-        <Switch>
-            <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Redirect to="/home" />
-        </Switch>
+        <Menu dishes={this.state.dishes} 
+              onClick= {(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail 
+        dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
         <Footer />
     </div>
     );
