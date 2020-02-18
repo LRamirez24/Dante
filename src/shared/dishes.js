0ef5438f2bb0000d65d2419a2aa40e44deff57a4
luis Ramirez <l.r.32@hotmail.com>
2020-02-18 13:10:06 -0800

    single page app part 2 with new images

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -2,42 +2,42 @@ export const DISHES =
     [
         {
         id: 0,
-        name:'Uthappizza',
-        image: '/assets/images/uthappizza.png',
+        name:'The Dante',
+        image: '/assets/images/pizza.jpg',
         category: 'mains',
         label:'Hot',
         price:'4.99',
         featured: true,
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+        description:'A unique combination of Italian pizza and Mexican sauces, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
         },
         {
         id: 1,
-        name:'Zucchipakoda',
-        image: '/assets/images/zucchipakoda.png',
+        name:'Calzone',
+        image: '/assets/images/calzone.jpg',
         category: 'appetizer',
         label:'',
         price:'1.99',
         featured: false,
-        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        description:'Deep fried Calzone coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
         },
         {
         id: 2,
-        name:'Vadonut',
-        image: '/assets/images/vadonut.png',
+        name:'Pizza Bagel',
+        image: '/assets/images/bagel.jpg',
         category: 'appetizer',
         label:'New',
         price:'1.99',
         featured: false,
-        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        description:'A quintessential Dante\'s experience, is it pizza or is it a bagel?'
         },
         {
         id: 3,
-        name:'ElaiCheese Cake',
-        image: '/assets/images/elaicheesecake.png',
-        category: 'dessert',
+        name:'The Alexander',
+        image: '/assets/images/alexander.jpg',
+        category: 'mains',
         label:'',
-        price:'2.99',
+        price:'4.99',
         featured: false,
-        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        description:'A delectable, sNew York Style salad packed with protein, with Graham cracker crust and spiced with Indian cardamoms'
         }
     ];
